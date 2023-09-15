e93cf1e8f61f6fead41d6a02196787168c93082e
Defou1ts <pasnkevikn@gmail.com>
2023-09-15 13:21:38 +0300

    feat: configured docker and connection to rabbitmq

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { Transport } from '@nestjs/microservices';
+
 import { AppModule } from './app.module';
 
+import type { MicroserviceOptions } from '@nestjs/microservices';
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+	const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+		transport: Transport.RMQ,
+		options: {
+			urls: ['amqp://admin:admin@rabbit:5672'],
+			queue: 'meetups_queue',
+			queueOptions: {
+				durable: false,
+			},
+		},
+	});
+
+	await app.listen();
 }
-bootstrap();
+void bootstrap();
